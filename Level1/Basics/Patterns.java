63943381573ce5b52870cb67cfcafe45fe290d6e
Nidhi pal <nidhipalmms@gmail.com>
2022-11-13 14:40:15 +0530

    Update Patterns.java

--- Level1/Basics/Patterns.java
+++ Level1/Basics/Patterns.java
@@ -170,18 +170,48 @@ public class Patterns {
 
     public static void pattern9(int n) {
 
+//         for (int i = 1; i <= n; i++) {
+
+//             for (int j = 1; j <= n; j++) {
+//                 if (i == j || i + j == n + 1)
+//                     System.out.print("*\t");
+//                 else
+//                     System.out.print("\t");
+//             }
+
+//             System.out.println();
+
+//         }
+        
+        int os=0, is=n-2;
+        
         for (int i = 1; i <= n; i++) {
 
-            for (int j = 1; j <= n; j++) {
-                if (i == j || i + j == n + 1)
-                    System.out.print("*\t");
-                else
-                    System.out.print("\t");
+            for (int j = 1; j <= os; j++) {
+                System.out.print("\t");
             }
-
+            System.out.print("*\t");
+            
+            for (int j = 1; j <= is; j++) {
+                System.out.print("\t");
+            }
+            if(i!=(n/2+1)) System.out.print("*\t");
+            
+            
             System.out.println();
+            
+            if(i<=n/2){
+                os++;
+                is-=2;
+            }else{
+                os--;
+                is+=2;
+            }
+            
 
         }
+        
+        
     }
 
     public static void pattern10(int n) {
