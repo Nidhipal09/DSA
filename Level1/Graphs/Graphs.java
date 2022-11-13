08f85771403d2e8d8067982bdddbbe2aec63a92e
Nidhi pal <nidhipalmms@gmail.com>
2022-11-13 16:01:27 +0530

    Update Graphs.java

--- Level1/Graphs/Graphs.java
+++ Level1/Graphs/Graphs.java
@@ -14,6 +14,22 @@ public class Graphs {
       this.wt = wt;
     }
   }
+  
+//       int vtces = Integer.parseInt(br.readLine());
+//       ArrayList<Edge>[] graph = new ArrayList[vtces];
+//       for(int i = 0; i < vtces; i++){
+//          graph[i] = new ArrayList<>();
+//       }
+
+//       int edges = Integer.parseInt(br.readLine());
+//       for(int i = 0; i < edges; i++){
+//          String[] parts = br.readLine().split(" ");
+//          int v1 = Integer.parseInt(parts[0]);
+//          int v2 = Integer.parseInt(parts[1]);
+//          int wt = Integer.parseInt(parts[2]);
+//          graph[v1].add(new Edge(v1, v2, wt));
+//          graph[v2].add(new Edge(v2, v1, wt));
+//       }
 
   public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] visited) {
 
